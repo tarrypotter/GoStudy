6ba4051f09b38ddfd6ef3f895b54a0f89eecc21c
LEE TaeWonng <109096999+tarrypotter@users.noreply.github.com>
2023-03-13 11:37:47 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  // go변수선언
+  // var 변수명 타입 = 초기값 
+  var a int =15
+  var b int =9
+  fmt.Println(a,b)
+  fmt.Println(a+b)
+  fmt.Println(a-b)
+  fmt.Println(a*b)
+  fmt.Println(a/b)
+  fmt.Println(a%b)
+}
