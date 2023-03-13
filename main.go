02c90ed3998b163e9292908c40edefaa8483aa9e
LEE TaeWonng <109096999+tarrypotter@users.noreply.github.com>
2023-03-13 11:49:02 +0000

    ㅍ

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
+//replit(사이트에서하는거랑,앱)에서 하는거라서 한파일만 run이 되는 바람에 이 파일은 항상 바뀔것
 
+//패키지 선언
 package main
-
+//패키지 가져오기
 import "fmt"
-
+//함수를 선언하고 중괄호로 시작을 알림 
 func main() {
+  // 출력하는 함수
+  fmt.Println("hi")
   // go변수선언
   // var 변수명 타입 = 초기값 
   var a int =15
