ed1e1d94d45f802be7902a1b314e05964c46ba91
LEE TaeWonng <109096999+tarrypotter@users.noreply.github.com>
2023-04-05 08:59:51 +0000

    d

---
+++ if_test01.go
@@ -0,0 +1,19 @@
+// if test
+
+package main
+
+//패키지 가져오기
+import "fmt"	
+
+
+//함수를 선언하고 중괄호로 시작을 알림 
+func if_test01() {  
+
+   Light := "red"
+
+  if Light =="green" {
+  fmt.Println("길을 건넌다")
+ }else {
+  fmt.Println("대기")
+    }
+  }
